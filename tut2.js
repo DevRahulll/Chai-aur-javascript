46fb16929d3241291aeeafb675427ab83fc9f19c
Rahul <rahuldevan2019@gmail.com>
2023-08-07 00:44:57 +0530

    about datatypes

---
+++ tut2.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js as newer version
+
+
+// alert(54-4) // we are using nodejs not browseer
+
+console.log(4+4);
+
+console.log("Rahul");
+
+let name="hitesh";
+let age=20;
+let islogged=true;
+let state;
+
+console.table([name,age,islogged,state]);
+
+// data types
+// number => 2 power53
+// bigInt
+// string -> " "
+// boolean => true/fale
+// null => standalone value
+// symbol => unique
+// object
+
+console.log(typeof islogged);
+
+console.table([typeof name,typeof age, typeof islogged, typeof state])
+
+console.log(typeof null);
+
