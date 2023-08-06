09345786d35ed348303ee05ef5491ffbc74349d5
Rahul <rahuldevan2019@gmail.com>
2023-08-07 00:17:05 +0530

    let const var eg

--- tut1.js
+++ tut1.js
@@ -1,1 +1,16 @@
 // let const and var
+const accountId=2348;
+let accountEmail="rahul@microsoft.com";
+var accountPassword="238457";
+ acccoutCity="Pune";
+ let accountState="Maharashtra";
+
+//  accountId=4;
+accountEmail="dev@gmail.com";
+ accountPassword="2345467";
+ acccoutCity="Jharkhand";
+
+ console.log(accountId);
+
+ console.table([accountEmail,accountId,accountState,accountPassword])
+
