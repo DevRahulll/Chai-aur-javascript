baca80b3d3f5c73d65e97e5b36af321a96602212
Rahul <rahuldevan2019@gmail.com>
2023-08-07 22:19:44 +0530

    adding tutorial of maths function

---
+++ tut4.js
@@ -0,0 +1,17 @@
+// Maths function or Methods
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.5555));
+// console.log(Math.ceil(4.4));
+
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random()*10)+10);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
