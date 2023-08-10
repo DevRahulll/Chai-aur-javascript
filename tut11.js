781b3caa78e9315bfcb0e7c20e8a0e782a67a416
Rahul <rahuldevan2019@gmail.com>
2023-08-11 00:36:39 +0530

    adding all the loops tutorial to the playlist

--- tut11.js
+++ tut11.js
@@ -1,1 +1,32 @@
-// LOOP => for , while ,
+// // LOOP => for , while ,do-while
+
+
+
+// // for (let i = 0; i < 10; i++) {
+// //     console.log(i);
+// // }
+
+
+
+let myarray=["ajooba","Krrish","Ra.One"]; 
+
+
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element);
+// }
+
+
+
+// let index=0;
+// while (index<myarray.length) {
+//     console.log(`Value of Index is ${myarray[index]}`);
+//     index++;
+// }
+
+
+let index=0;
+do {
+    console.log(`value of index is ${myarray[index]}`);
+    index++;
+} while (index<myarray.length);
