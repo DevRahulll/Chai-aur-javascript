c65611afdf6f454ffd27b837d25cfc6f01fb8e1c
Rahul <rahuldevan2019@gmail.com>
2023-08-13 23:56:05 +0530

    adding project 4 with all corrections

--- Projects/4-Guress the number/sctipt.js
+++ Projects/4-Guress the number/sctipt.js
@@ -47,28 +47,47 @@ function validateGuess(guess) {
 
 function checkGuess() {
     if (guessSlot === randomNumber) {
-        displayMessage(`You guessed it right : ${randomNumber}`)
+        displayMessage(`You guessed it right`)
         endGame();
     } else if (guess < randomNumber) {
         displayMessage(`Number is toooo Low`)
     } else if (guess > randomNumber) {
         displayMessage(`Number is tooo High`)
-
     }
 }
 
-function displayGuess() {
-
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}, `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
 }
 
-function displayMessage() {
-
+function displayMessage(message) {
+    loworHi.innerHTML = `<h2>${message}</h2>`;
 }
 
 function endGame() {
-
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2  id="newGame">Start New Game</h2>`;
+    StartOver.appendChild(p);
+    playGame = false;
+    newGame();
 }
 
 function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function (e) {
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess}`
+        userInput.removeAttribute('disabled');
+        StartOver.removeChild(p);
 
-}
+        playGame = true;
+    })
+};
