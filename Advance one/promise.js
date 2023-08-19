fc55e8057c9527c34d7a7aff1402758d1acb3fc3
Rahul <rahuldevan2019@gmail.com>
2023-08-19 22:28:56 +0530

    adding Promise tutorial to the playlist

---
+++ Advance one/promise.js
@@ -0,0 +1,19 @@
+const PromiseOne=new Promise(function(resolve,reject){
+    setTimeout(function() {
+        console.log("Async Task is complete");
+        resolve();
+    }, 1000);
+})
+
+PromiseOne.then(function(){
+    console.log("promise consumed");
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function() {
+        console.log("Async task 2 ");
+        resolve();
+    }, 1000);
+}).then(function(){
+    console.log("Async 2 resolved");
+})
