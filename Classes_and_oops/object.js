fc80009b7a405ddc0f675893a7d7e15a9177fada
Rahul <rahuldevan2019@gmail.com>
2023-08-23 23:25:43 +0530

    adding additon to the object in the javascript series

--- Classes_and_oops/object.js
+++ Classes_and_oops/object.js
@@ -11,6 +11,13 @@ function createUser(username,score){
     this.username=username
     this.score=score
 }
-
+createUser.prototype.increment=function(){
+    this.score++
+}
+createUser.prototype.printMe=function(){
+    console.log(`price is${this.score}`);
+}
 const chai=new createUser("dev",46);
 const tea=createUser("mahato",64)
+
+chai.printMe();
